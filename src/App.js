ff5f4fcd3d14dbc71321739500e0fed67c06c191
moondrop0816 <moondrop0816@gmail.com>
2023-03-28 00:05:40 +0900

    add: 라우터 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import GlobalStyle from "./style/GlobalStyle";
+import Header from "./Header";
+import { Routes, Route } from "react-router-dom";
+import TodoContainer from "./pages/TodoContainer";
+import Statistics from "./pages/Statistics";
+import MakerInfo from "./pages/MakerInfo";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div className="App">투두앱을 만들거에용</div>
+      <div className="App">
+        <Header />
+        <section className="wrapper">
+          <Routes>
+            <Route
+              path="/"
+              element={<TodoContainer />}
+            />
+            <Route
+              path="/statistics"
+              element={<Statistics />}
+            />
+            <Route
+              path="/makerinfo"
+              element={<MakerInfo />}
+            />
+          </Routes>
+        </section>
+      </div>
     </>
   );
 }
