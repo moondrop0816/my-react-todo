78885ed6f1233525977923f309f770a08d745226
moondrop0816 <moondrop0816@gmail.com>
2023-03-30 23:56:43 +0900

    add: 스타일 및 마크업 수정

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,15 @@
 import Nav from "./Nav";
 import styled from "styled-components";
+import { useState } from "react";
 
 const StyledHeader = styled.header`
   width: 100%;
   display: flex;
   justify-content: center;
   background-color: var(--white);
+  position: fixed;
 
-  & > div {
+  .header-btns {
     width: 90%;
     display: flex;
     justify-content: flex-end;
@@ -16,20 +18,29 @@ const StyledHeader = styled.header`
 `;
 
 const Header = () => {
-  // header
-  // nav와 다크모드 버튼 포함
+  const [nav, setNav] = useState(false);
+  const handleMenu = () => {
+    setNav(!nav);
+  };
+
   return (
     <StyledHeader>
-      <div>
+      <div className="header-btns">
         <button type="button">
           <span className="material-icons-round">light_mode</span>
           <span className="material-icons-round">dark_mode</span>
         </button>
-        <button type="button">
+        <button
+          type="button"
+          onClick={handleMenu}
+        >
           <span className="material-icons-round">menu</span>
         </button>
       </div>
-      {/* <Nav /> */}
+      <Nav
+        menuOpen={nav}
+        handleMenu={handleMenu}
+      />
     </StyledHeader>
   );
 };
--- src/Nav.js
+++ src/Nav.js
@@ -1,22 +1,80 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Icon from "./style/Icons";
 
-const Nav = () => {
+const StyledNavBg = styled.div`
+  width: 100%;
+  height: 100vh;
+  opacity: 0;
+  background: rgba(0, 0, 0, 0.5);
+  position: absolute;
+  top: 0;
+  right: 0;
+  transition: all 0.5s;
+  z-index: -1;
+
+  &.open {
+    opacity: 1;
+    right: 0;
+    z-index: 9;
+  }
+`;
+const StyledNav = styled.nav`
+  width: 50%;
+  height: 100%;
+  padding: 1em;
+  background: var(--white);
+  position: fixed;
+  top: 0;
+  right: -100%;
+  display: flex;
+  flex-direction: column;
+  z-index: 9;
+  transition: all 0.5s;
+
+  .btn-nav-close {
+    align-self: flex-end;
+  }
+
+  li {
+    margin: 1em 0;
+  }
+
+  &.open {
+    right: 0;
+  }
+`;
+
+const Nav = ({ menuOpen, handleMenu }) => {
   // 메뉴들이 들어가는 네비게이션
   // 오늘의 할일, 통계, 만든사람
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">오늘의 할일</Link>
-        </li>
-        <li>
-          <Link to="/statistics">통계 보기</Link>
-        </li>
-        <li>
-          <Link to="/makerinfo">만든 사람</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <StyledNavBg
+        className={menuOpen ? "open" : ""}
+        onClick={handleMenu}
+      />
+      <StyledNav className={menuOpen ? "open" : ""}>
+        <button
+          type="button"
+          className="btn-nav-close"
+          onClick={handleMenu}
+        >
+          <Icon name="close" />
+        </button>
+        <ul>
+          <li>
+            <Link to="/">오늘의 할일</Link>
+          </li>
+          <li>
+            <Link to="/statistics">통계 보기</Link>
+          </li>
+          <li>
+            <Link to="/makerinfo">만든 사람</Link>
+          </li>
+        </ul>
+      </StyledNav>
+    </>
   );
 };
 
