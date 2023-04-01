292e7e826b150ea98991fed9b587fecccba55f48
moondrop0816 <moondrop0816@gmail.com>
2023-04-01 15:37:27 +0900

    feat: 날짜표시, 할일 데이터 조회

--- src/pages/TodoContainer.js
+++ src/pages/TodoContainer.js
@@ -1,13 +1,44 @@
 import Todo from "../components/Todo";
 import TodoCreateModal from "../components/TodoCreateModal";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const TodoContainer = () => {
   // Todo 컴포넌트들을 감싼 페이지
+  const [todos, setTodos] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get("http://localhost:3001/todos");
+      setTodos(data);
+    })();
+  }, []);
+
+  const getDate = () => {
+    const d = new Date();
+    const week = [
+      "일요일",
+      "월요일",
+      "화요일",
+      "수요일",
+      "목요일",
+      "금요일",
+      "토요일",
+    ];
+
+    const year = d.getFullYear();
+    const month = d.getMonth() + 1;
+    const date = d.getDate();
+    const day = week[d.getDay()];
+
+    return `${year}년 ${month}월 ${date}일 ${day}`;
+  };
+
   return (
     <div>
       <div>
-        <div>2023년 04월 07일 금요일</div>
-        <div>남은 할일 00개</div>
+        <h2>{getDate()}</h2>
+        <h3>남은 할일 00개</h3>
       </div>
       <div>
         <select>
@@ -16,7 +47,7 @@ const TodoContainer = () => {
           <option value="완료">완료</option>
         </select>
         <button type="button">옵션</button>
-        <div>
+        <div className="todo-options">
           <ul>
             <li>완료한 할일만 삭제</li>
             <li>전체 삭제</li>
@@ -24,7 +55,14 @@ const TodoContainer = () => {
         </div>
       </div>
       <ul>
-        <Todo />
+        {todos.map((todo) => {
+          return (
+            <Todo
+              key={todo.id}
+              todo={todo}
+            />
+          );
+        })}
       </ul>
       <TodoCreateModal />
       <button type="button">추가</button>
