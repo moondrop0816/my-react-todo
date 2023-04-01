3f9f1640f3cb69f4abe24287b6a5980b5f1b2f46
moondrop0816 <moondrop0816@gmail.com>
2023-04-01 15:36:28 +0900

    comment: 할일 주석추가

--- src/Header.js
+++ src/Header.js
@@ -27,6 +27,7 @@ const Header = () => {
     <StyledHeader>
       <div className="header-btns">
         <button type="button">
+          {/* Todo: 라이트모드 / 다크모드 styled-component ThemeProvider로 처리할것 */}
           <span className="material-icons-round">light_mode</span>
           <span className="material-icons-round">dark_mode</span>
         </button>
