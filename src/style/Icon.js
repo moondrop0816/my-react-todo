6fadfc84d7a33bb1a1e79f765565f16a0fe476e5
moondrop0816 <moondrop0816@gmail.com>
2023-04-02 19:48:14 +0900

    feat: fontSize, color props 추가

---
+++ src/style/Icon.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+const StyledIcon = styled.span`
+  font-family: "Material Icons";
+  font-weight: normal;
+  font-style: normal;
+  font-size: ${(props) => props.fontSize || "2.4rem"}; /* Preferred icon size */
+  color: ${(props) => props.color || "#333"};
+  display: inline-block;
+  line-height: 1;
+  text-transform: none;
+  letter-spacing: normal;
+  word-wrap: normal;
+  white-space: nowrap;
+  direction: ltr;
+
+  /* Support for all WebKit browsers. */
+  -webkit-font-smoothing: antialiased;
+  /* Support for Safari and Chrome. */
+  text-rendering: optimizeLegibility;
+
+  /* Support for Firefox. */
+  -moz-osx-font-smoothing: grayscale;
+
+  /* Support for IE. */
+  font-feature-settings: "liga";
+`;
+
+const Icon = ({ name, fontSize, color }) => {
+  return (
+    <StyledIcon
+      className="material-icons-round"
+      fontSize={fontSize}
+      color={color}
+    >
+      {name}
+    </StyledIcon>
+  );
+};
+
+export default Icon;
