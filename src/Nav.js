e1ca1fb4fb79246ddd16e97f57fbe0f8bdfd4c6c
moondrop0816 <moondrop0816@gmail.com>
2023-04-02 19:44:38 +0900

    fix: Icon 파일 링크 수정

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import Icon from "./style/Icons";
+import Icon from "./style/Icon";
 
 const StyledNavBg = styled.div`
   width: 100%;
