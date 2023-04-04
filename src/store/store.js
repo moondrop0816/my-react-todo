c9544e4b27e5d4f39e0d926ffe880c8242d24593
moondrop0816 <moondrop0816@gmail.com>
2023-04-04 22:12:31 +0900

    feat: 리덕스 스토어 설정 및 redux-thunk 미들웨어 추가

--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,8 @@
 // redux store
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import thunk from "redux-thunk";
+import TodoReducer from "../reducers/todoReducer";
+
+const store = createStore(TodoReducer, applyMiddleware(thunk));
+
+export default store;
