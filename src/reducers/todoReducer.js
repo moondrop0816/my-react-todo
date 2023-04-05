5139770d8bcefe69639f7a15b80d8f272b8b7a62
moondrop0816 <moondrop0816@gmail.com>
2023-04-05 21:35:48 +0900

    feat: todo 등록 구현

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,31 +1,50 @@
-import { GET_TODOS, GET_TODOS_ERROR, GET_TODOS_SUCCESS } from "../actions";
+import {
+  ADD_TODO,
+  ADD_TODO_ERROR,
+  ADD_TODO_SUCCESS,
+  GET_TODOS,
+  GET_TODOS_ERROR,
+  GET_TODOS_SUCCESS,
+} from "../actions";
 import { initialState } from "./initialState";
 
 const TodoReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_TODOS:
       return {
-        todoData: {
-          loading: true,
-          data: [],
-          error: null,
-        },
+        loading: true,
+        data: [],
+        error: null,
       };
     case GET_TODOS_SUCCESS:
       return {
-        todoData: {
-          loading: false,
-          data: action.data,
-          error: null,
-        },
+        loading: false,
+        data: action.data,
+        error: null,
       };
     case GET_TODOS_ERROR:
       return {
-        todoData: {
-          loading: true,
-          data: [],
-          error: action.error,
-        },
+        loading: true,
+        data: [],
+        error: action.error,
+      };
+    case ADD_TODO:
+      return {
+        loading: true,
+        data: [...state.data],
+        error: null,
+      };
+    case ADD_TODO_SUCCESS:
+      return {
+        loading: false,
+        data: [...state.data, action.payload],
+        error: null,
+      };
+    case ADD_TODO_ERROR:
+      return {
+        loading: true,
+        data: [],
+        error: action.error,
       };
     default:
       return state;
